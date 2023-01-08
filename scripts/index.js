eeeb091f9ad95158febc1abb306ac9032d05425d
rastereo <kapturhouse@yandex.ru>
2023-01-09 01:36:17 +0300

    feat: add delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let editButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
@@ -34,40 +61,17 @@ editButton.addEventListener('click', actionPopup);
 closeButton.addEventListener('click', actionPopup);
 formPopup.addEventListener('submit', rewriteProfileInfo);
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-initialCards.forEach(item => {
+function renderCard (item) {
   const cards = document.querySelector('.cards');
   const copyTemplateCards = document.querySelector('#template-cards').content.cloneNode(true);
+  const card = copyTemplateCards.querySelector('.card');
 
-  copyTemplateCards.querySelector('.card__description').textContent = `${item.name}`;
+  copyTemplateCards.querySelector('.card__description').textContent = item.name;
   copyTemplateCards.querySelector('.card__image').src = item.link;
-  copyTemplateCards.querySelector('.card__image').alt = `${item.name}`;
+  copyTemplateCards.querySelector('.card__image').alt = item.name;
+  copyTemplateCards.querySelector('.card__delete-button').addEventListener('click', () => card.remove());
 
   cards.append(copyTemplateCards);
-});
+}
+
+initialCards.forEach(renderCard);
