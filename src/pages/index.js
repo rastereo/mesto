52421fe58ba9a587bfa081a29ea18ba489e836d2
rastereo <kapturhouse@yandex.ru>
2023-03-12 00:40:39 +0300

    refactor: connect webpack in project

---
+++ src/pages/index.js
@@ -0,0 +1,96 @@
+import '../pages/index.css';
+
+import {
+  editButton,
+  addButton,
+  popupEditProfileForm,
+  popupAddImageForm
+} from '../utils/constants.js';
+
+import { initialCards } from '../utils/initialCards.js';
+import { formValidationConfig } from '../utils/formValidationConfig.js';
+
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+
+const userInfo = new UserInfo({
+  nameSelector: '.profile__name',
+  jobSelector: '.profile__job'
+})
+
+const formValidatorEditProfile = new FormValidator(formValidationConfig, popupEditProfileForm);
+const formValidatorAddImage = new FormValidator(formValidationConfig, popupAddImageForm);
+
+formValidatorEditProfile.enableValidation();
+formValidatorAddImage.enableValidation();
+
+function createCard(item) {
+  const card = new Card({
+    data: item,
+    handleCardClick: (name, link) => {
+      const popupWhithImage = new PopupWithImage({ name, link }, '.popup_name_image');
+
+      popupWhithImage.setEventListeners();
+      popupWhithImage.open();
+    }
+  }, '#template-cards');
+
+  const cardElement = card.generateCard();
+
+  return cardElement;
+}
+
+const cardList = new Section({
+  items: initialCards,
+  renderer: (item) => {
+    cardList.addItem(createCard(item));
+  }
+}, '.cards');
+
+cardList.renderItems();
+
+const popupEditProfile = new PopupWithForm({
+  handleFormSubmit: ({ name, job }) => {
+    userInfo.setUserInfo({ name, job });
+
+    popupEditProfile.close();
+  }
+}, '.popup_name_edit-profile')
+
+popupEditProfile.setEventListeners();
+
+const popupAddImage = new PopupWithForm({
+  handleFormSubmit: ({ description, link }) => {
+    const cardObject = new Object()
+
+    cardObject.name = description;
+    cardObject.link = link;
+
+    cardList.addItem(createCard(cardObject));
+
+    popupAddImage.close();
+  }
+}, '.popup_name_add-image');
+
+popupAddImage.setEventListeners();
+
+editButton.addEventListener('click', () => {
+  const { name, job } = userInfo.getUserInfo()
+
+  popupEditProfileForm.name.value = name;
+  popupEditProfileForm.job.value = job;
+
+  formValidatorEditProfile.resetValidation();
+
+  popupEditProfile.open();
+});
+
+addButton.addEventListener('click', () => {
+  formValidatorAddImage.resetValidation();
+
+  popupAddImage.open();
+});
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const popupEditProfileForm = document.querySelector('.popup_name_edit-profile').querySelector('.popup__form');
+const popupAddImageForm = document.querySelector('.popup_name_add-image').querySelector('.popup__form');
+
+export {
+  editButton,
+  addButton,
+  popupEditProfileForm,
+  popupAddImageForm
+}
